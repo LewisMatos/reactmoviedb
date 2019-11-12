a7cd7f67176455372ab36c41268166de31710e49
Lewis <lewm20@gmail.com>
2019-11-12 11:57:47 -0500

    update styles

--- src/style/StyledGridItems.js
+++ src/style/StyledGridItems.js
@@ -1,10 +1,10 @@
 import styled from 'styled-components'
 export const StyledGridItems = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(450px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
   grid-gap: 40px;
 
-  @media only screen and (max-width: 1000px) {
-    grid-template-columns: repeat(auto-fit, minmax(90px, 1fr));
+  @media only screen and (max-width: 1200px) {
+    grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
   }
 `
