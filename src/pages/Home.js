b04ae2cd23e53dbaca63a9dbced8a0799525fa75
LewisMatos <lewm20@gmail.com>
2019-11-25 05:01:41 -0500

    refactor to hooks

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,79 +1,87 @@
-import React, { Component } from 'react'
+import React, { Component, useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import 'normalize.css'
 
 import MovieGrid from '../components/MovieGrid'
 import Header from '../components/Header'
-import { StaticQuery } from 'gatsby'
+// import { StaticQuery } from 'gatsby'
 import MovieCard from '../components/MovieCard'
+import { useSiteMetadata } from '../hooks/useSiteMetaData'
 
-class Home extends React.Component {
-  state = {
-    movies: [],
-    endpoint: '',
-  }
+const Home = () => {
+  const [movies, setMovie] = useState([])
+  const [endpoint, setEndpoing] = useState('')
 
-  getMovies = async endpoint => {
+  const { title, top_rated_endpoint, popular_endpoint, image_url, image_size } = useSiteMetadata()
+
+  const getMovies = async endpoint => {
     try {
       const res = await fetch(endpoint)
       const data = await res.json()
-      this.setState(prev => ({ ...prev, movies: [...data.results] }))
-
-      sessionStorage.setItem('movies', JSON.stringify(this.state))
+      console.log('getmovie')
+      setMovie(prev => [...data.results] )
     } catch (error) {
       console.log(error)
     }
   }
 
-  componentDidMount() {
+
+
+  useEffect(() => {
     if (sessionStorage.movies) {
-      this.setState(JSON.parse(sessionStorage.movies))
+      setMovie(JSON.parse(sessionStorage.movies))
       return
     }
-    // console.log("here");
-    const { popular_endpoint, top_rated_endpoint } = this.props.site.siteMetadata
-    this.getMovies(popular_endpoint)
-  }
+    getMovies(popular_endpoint)
+  }, [])
 
-  render() {
-    const { title, image_url, image_size } = this.props.site.siteMetadata
-    return (
-      <>
-        <Header siteTitle={title} getMovies={this.getMovies} />
-        <MovieGrid>
-          {this.state.movies.map(movie => {
-            return (
-              <MovieCard key={movie.id} movieId={movie.id} image={`${image_url}${image_size}${movie.poster_path}`} />
-            )
-          })}
-        </MovieGrid>
-      </>
-    )
-  }
+
+  useEffect(() => {
+    sessionStorage.setItem('movies', JSON.stringify(movies))
+  }, [movies])
+
+  // componentDidMount() {
+  // if (sessionStorage.movies) {
+  //   this.setState(JSON.parse(sessionStorage.movies))
+  //   return
+  // }
+  // console.log("here");
+  // const { popular_endpoint, top_rated_endpoint } = this.props.site.siteMetadata
+  // this.getMovies(popular_endpoint)
+  // }
+  return (
+    <>
+      <Header siteTitle={title} getMovies={getMovies} />
+      <MovieGrid>
+        {movies.map(movie => {
+          return <MovieCard key={movie.id} movieId={movie.id} image={`${image_url}${image_size}${movie.poster_path}`} />
+        })}
+      </MovieGrid>
+    </>
+  )
 }
 
 Home.propTypes = {
   siteMetaData: PropTypes.object,
 }
 
+// export default () => (
+//   <StaticQuery
+//     query={graphql`
+//       query {
+//         site {
+//           siteMetadata {
+//             title
+//             top_rated_endpoint
+//             popular_endpoint
+//             image_url
+//             image_size
+//           }
+//         }
+//       }
+//     `}
+//     render={data => <Home site={data.site} />}
+//   />
+// )
 
-export default () => (
-  <StaticQuery
-    query={graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            top_rated_endpoint
-            popular_endpoint
-            image_url
-            image_size
-          }
-        }
-      }
-    `}
-    render={data => <Home site={data.site} />}
-  />
-)
-
-
+export default Home
